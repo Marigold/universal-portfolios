c3b217b1f7dbd80fedd5ce6fde2854ff07d8e5c8
Mojmir Vinkler <mojmir.vinkler@gmail.com>
2015-04-26 11:32:40 +0200

    use py.test instead of unittests & add requirements.txt

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt')
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name='universal-portfolios',
       version='0.1',
       description='Collection of algorithms for online portfolio selection',
-      url = 'https://github.com/Marigold/universal-portfolios',
-      download_url = 'https://github.com/marigold/universal-portfolios/tarball/0.1',
+      url='https://github.com/Marigold/universal-portfolios',
+      download_url='https://github.com/marigold/universal-portfolios/tarball/0.1',
       author='Mojmir Vinkler',
       author_email='mojmir.vinkler@gmail.com',
       license='MIT',
       packages=find_packages(),
       package_data={
-      	'universal': ['data/*.pkl']
+            'universal': ['data/*.pkl']
       },
       keywords=['portfolio'],
-      install_requires = ['pandas>=0.13.1', 'cvxopt>=1.1.6', 'matplotlib>=1.3.1'],
+      install_requires=reqs,
       zip_safe=False)
---
+++ universal/tests/test_algos.py
@@ -0,0 +1,50 @@
+import pytest
+from universal import algos
+import universal.tools as tools
+
+
+EPS = 1e-10
+
+
+@pytest.fixture(scope="module")
+def S():
+    """ Random portfolio for testing. """
+    return tools.random_portfolio(n=1000, k=3, mu=0., sd=0.01)
+
+
+@pytest.mark.parametrize("algo_class", [
+    algos.CRP,
+    algos.RMR,
+    algos.OLMAR,
+    algos.PAMR,
+])
+def test_bias(algo_class, S):
+    """ Test forward bias of algo. Test on a portion of given data set, then add several
+    data points and see if weights has changed. """
+    m = 10
+    B1 = algo_class().run(S.iloc[:-m]).B
+    B2 = algo_class().run(S).B
+
+    assert (B1 == B2.iloc[:-m]).all().all()
+
+
+# BAH
+def test_bah(S):
+    """ Fees for BAH should be equal to 1 * fee. """
+    FEE = 0.01
+    result = algos.BAH().run(S)
+    wealth_no_fees = result.total_wealth
+    result.fee = FEE
+    wealth_with_fees = result.total_wealth
+
+    assert abs(wealth_no_fees * (1 - FEE) - wealth_with_fees) < EPS
+
+
+# CRP
+def test_crp(S):
+    """ Make sure that equity of a portfolio [1,0,...,0] with NaN values
+    is the same as asset itself. """
+    b = [1.] + [0.] * (len(S.columns) - 1)
+    result = algos.CRP(b).run(S)
+
+    assert abs(result.total_wealth - S[S.columns[0]].iget(-1)) < EPS
